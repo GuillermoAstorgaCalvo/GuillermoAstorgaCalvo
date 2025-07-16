0e84a138cc780e0c819316a2be325a3422a7311f
Guillermo Astorga <guillermoastorgacalvo@gmail.com>
2025-07-16 22:58:03 +0200

    updated dependencies

--- scripts/update_dependencies.py
+++ scripts/update_dependencies.py
@@ -103,7 +103,6 @@ class DependencyManager:
                     "mypy",
                     "bandit",
                     "safety",
-                    "pre-commit",
                 ],
             )
         )
